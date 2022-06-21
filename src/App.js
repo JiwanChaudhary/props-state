ce3950711bcc27e4df009b8257e6d8fa614abff0
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-06-21 22:41:23 +0545

    learning props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import New from './New';
 
 function App() {
-  return <New />;
+  return (
+    <>
+    <New text="Learn React"/>
+    <New text="Master React"/>
+    <New text="Teach React"/>
+    </>
+  );
 }
 
 export default App;
--- src/New.jsx
+++ src/New.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 
-const New = () => {
-  return <div>New</div>;
-};
+function New(props) {
+  const {text} = props;
+  const [newText, setNewText] = useState(text);
+
+  function handleClick() {
+    setNewText('Learning Props');
+  }
+  return (
+    <section>
+      <p>{newText}</p>
+      <button type="submit" onClick={handleClick}>
+        Click Me
+      </button>
+    </section>
+  );
+}
 
 export default New;
